32bc8318bacfe93d1835dd66ea60b1f03ae8fcfa
Kevin Kulich <klpv733@gmail.com>
2023-06-04 11:03:56 -0600

    Load data from firestore Moving from daisyUI html to components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import { getFirestore, collection, doc, setDoc, serverTimestamp } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useIsAuthenticated } from '@/hooks/auth';
+import { v4 as uuid } from 'uuid';
+
+type AccordionOpen = 'tasks' | 'reminders' | 'events' | 'none';
 
 function Dashboard() {
-	return <div>Hello</div>;
+	const { user, loading, error } = useIsAuthenticated();
+	const [open, setOpen] = useState<AccordionOpen>('tasks');
+	const [data, dataLoading, dataError] = useCollectionData(
+		collection(getFirestore(), 'users', user?.uid ?? 'none', 'tasks')
+	);
+
+	const addCollection = async () => {
+		const id = uuid();
+		await setDoc(doc(getFirestore(), `users/${user!.uid}/tasks/${id}`), {
+			name: 'Test',
+			id: id,
+			description: 'My description',
+			timestamp: serverTimestamp(),
+		});
+	};
+	return (
+		<>
+			{dataLoading ? (
+				<div>Loading</div>
+			) : (
+				<>
+					{data && data.length > 0 ? (
+						<>Data!!!</>
+					) : (
+						<div className='flex flex-col space-y-4'>
+							<span>Sad</span>
+						</div>
+					)}{' '}
+					<button className='btn btn-primary w-40' onClick={addCollection}>
+						Add me
+					</button>
+				</>
+			)}
+		</>
+	);
 }
 
 export default Dashboard;
--- hooks/auth.ts
+++ hooks/auth.ts
@@ -1,5 +1,9 @@
+'use client';
+
 import { startApp } from '@/util/firebase';
 import { getAuth } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 /**
@@ -10,4 +14,18 @@ export function useIsAuthenticated() {
 	startApp(); // Will only call if there's no apps installed
 	const [user, loading, error] = useAuthState(getAuth());
 	return { user: user ?? null, loading, error };
+}
+
+/**
+ * Hook for protecting authenticated routes
+ * @returns useEffect to redirect user to homepage if not logged in
+ */
+export function useProtectedRoute() {
+	const router = useRouter();
+	const { user, loading, error } = useIsAuthenticated();
+	return useEffect(() => {
+		if (!user && (!loading || error)) {
+			router.push('/');
+		}
+	}, [user, loading, error, router]);
 }
